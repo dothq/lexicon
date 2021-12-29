627fcb3b919c8181d14bd1d783f2899a8535b4f4
EnderDev <42723993+EnderDev@users.noreply.github.com>
2021-12-29 15:49:47 +0000

    🚚 Move frontend to dothq/lexicon-frontend

--- frontend/components/CardHeader/style.ts
+++
@@ -1,9 +0,0 @@
-import styled from "@emotion/styled";
-
-export const CardHeader = styled.header`
-    width: 100%;
-    height: 56px;
-    border-bottom: 1px solid rgba(0, 0, 0, 0.1);
-    display: flex;
-    align-items: center;
-`;
--- frontend/components/IconButton/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import { StyledIconButton } from "./style"
-
-export const IconButton = (props: any) => {
-    return (
-        <StyledIconButton {...props} />
-    )
-}
